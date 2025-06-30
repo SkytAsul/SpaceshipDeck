4bb7eabfeddd7d11e738148e78a2fa48efccbce9
SkytAsul <skytasul@gmail.com>
2025-06-30 22:30:22 +0200

    fix(main_computer): sigint no longer prevents exit

--- packages/main_computer/bin/boot.dart
+++ packages/main_computer/bin/boot.dart
@@ -1,60 +1,12 @@
 import 'dart:async';
 import 'dart:io';
 
-import 'package:args/args.dart';
 import 'package:main_computer/main_computer.dart';
 import 'package:main_computer/src/utils/async.dart';
 
-ArgParser buildParser() {
-  return ArgParser()
-    ..addFlag(
-      'help',
-      abbr: 'h',
-      negatable: false,
-      help: 'Print this usage information.',
-    )
-    ..addFlag(
-      'verbose',
-      abbr: 'v',
-      negatable: false,
-      help: 'Show additional command output.',
-    )
-    ..addOption("agent-token", defaultsTo: Platform.environment["AGENT_TOKEN"]);
-}
+StreamSubscription? signalStreamSubscription;
 
-void printUsage(ArgParser argParser) {
-  print('Usage: dart main_computer.dart <flags> [arguments]');
-  print(argParser.usage);
-}
-
-Future<void> main(List<String> arguments) async {
-  final ArgParser argParser = buildParser();
-  /*MainComputerInit computer;
-  try {
-    final ArgResults results = argParser.parse(arguments);
-
-    // Process the parsed arguments.
-    if (results.flag('help')) {
-      printUsage(argParser);
-      return;
-    }
-
-    final agentToken = results.option("agent-token");
-    if (agentToken == null) {
-      throw ArgumentError("agent_token not found.");
-    }
-
-    computer = MainComputerInit(
-      verbose: results.flag('verbose'),
-      agentToken: agentToken,
-    );
-  } on FormatException catch (e) {
-    print(e.message);
-    print('');
-    printUsage(argParser);
-    exit(2);
-  }*/
-
+Future<void> main() async {
   final kernel = await bootKernel();
   
   if (stdin.hasTerminal) {
@@ -64,10 +16,15 @@ Future<void> main(List<String> arguments) async {
   _catchSigint(kernel);
   
   await kernel.run();
+  print("Kernel shut down.");
+  
+  await signalStreamSubscription?.cancel();
 }
 
 void _catchSigint(SpaceshipKernel kernel) {
-  ProcessSignal.sigint.watch().next().then((_) async {
+  ProcessSignal.sigint.watch().asBroadcastStream(
+    onListen: (subscription) => signalStreamSubscription = subscription,
+  ).next().then((_) async {
     print("\nReceived SIGINT, shutting down.");
     kernel.askShutdown();
   });
