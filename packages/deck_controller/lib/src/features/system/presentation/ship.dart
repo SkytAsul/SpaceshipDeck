a0231063a163276dbc77d02b74589d8ac0093e9d
SkytAsul <skytasul@gmail.com>
2026-07-22 22:22:06 +0200

    feat(deck_controller): better ship icon and tooltip

--- packages/deck_controller/lib/src/features/system/presentation/ship.dart
+++ packages/deck_controller/lib/src/features/system/presentation/ship.dart
@@ -1,21 +1,40 @@
 import 'package:commons/commons.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' show Tooltip;
 import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:forui/forui.dart';
 
 class ShipWidget extends StatelessWidget {
   final Ship ship;
 
-  static const radius = 5;
+  static const radius = 4.0;
 
   const ShipWidget(this.ship, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: radius * 2,
-      height: radius * 2,
-      decoration: BoxDecoration(shape: .circle, color: Colors.yellow),
-      child: Icon(Icons.airplanemode_active),
+    return GestureDetector(
+      onTap: () {
+        // SystemMapState.of(context).togglePopup(widget.waypoint.symbol);
+      },
+      child: Tooltip(
+        message: ship.symbol,
+        child: Container(
+          width: radius * 2,
+          height: radius * 2,
+          padding: .zero,
+          decoration: BoxDecoration(
+            shape: .circle,
+            color: context.theme.colors.primary.withAlpha(150),
+          ),
+          alignment: .center,
+          child: FaIcon(
+            FontAwesomeIcons.rocket,
+            size: radius * 1.2,
+            color: context.theme.colors.primaryForeground,
+          ),
+        ),
+      ),
     );
   }
 }
